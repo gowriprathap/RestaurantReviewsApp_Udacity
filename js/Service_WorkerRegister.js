3e4145317ef3bca093d8746490063ae0b66314d7
gowriprathap <gowrinp@gmail.com>
2019-05-15 23:02:48 -0400

    Editing changes

--- js/Service_WorkerRegister.js
+++ js/Service_WorkerRegister.js
@@ -1,12 +1,11 @@
 //js file to register the service worker
-if ('serviceWorker' in navigator){ //this if statement is only executed if browser supports the service worker, else skips this statement
+if ('serviceWorker' in navigator) { //this if statement is only executed if browser supports the service worker, else skips this statement
   navigator.serviceWorker.register('./sw.js')
-  .then(function(){
+  .then(function() {
 console.log('Registration worked'); //logging a success message if the service worker registration is successful
 })
- .catch(function(){
+ .catch(function() {
 console.log('Registration failed'); //logging a failure message if the service worker registration failed
 });
-
 }
 //service worker only works on HTTPS
